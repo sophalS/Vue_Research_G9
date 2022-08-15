0acf1c6af0808062330b923bd5225be706b91309
Kea Nhor <90302802+Keanhor2@users.noreply.github.com>
2022-08-15 07:45:11 +0700

    Add files via upload

---
+++ vue-tailwind/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav-view/>
+    <card-view/>
+    <card-view/>
+    <card-view/>
+    <card-view/>
+</template>
+
+<script>
+import Header from './components/HeaderView.vue'
+import Card from './components/CardView.vue'
+export default {
+   components:{
+      "card-view":Card,
+      "nav-view":Header
+   }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-tailwind/src/components/CardView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="card border-sky-300 border-x-8 border-y-2 mx-5 my-3 flex justify-between items-center ">
+        <div class="subCard">
+            <div class="card-title ">
+                <img class=" w-20 bg-gradient-to-r from-sky-500 to-indigo-500 bg-center p-2 rounded-full " src="../assets/logo.png" alt="" >
+            </div>
+            <div class="card-body">
+                <p class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500"> The King is the most powerful</p>
+            </div>
+        </div>
+        <div class="btn">
+            <button class="py-1 px-3 border bg-sky-400 mx-2 my-2 rounded-lg ">Update</button>
+            <button class="py-1 px-3 border  bg-red-500 rounded-lg ">Delete</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+    }
+</script>
+
+<style>
+    @import url("../assets/index.css")
+
+</style>
